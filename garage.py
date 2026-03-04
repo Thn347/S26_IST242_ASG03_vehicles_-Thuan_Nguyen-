62003da59f818746840416d225d521cc6808ee68
Thn347 <thn5129@psu.edu>
2026-03-04 14:56:03 -0500

    Added new "garage.py"

---
+++ garage.py
@@ -0,0 +1,28 @@
+from vehicle import Vehicle
+
+class Garage:
+    '''
+    A garage is a place that stores a collection of vehicles
+    '''
+
+    # Constructor
+    def __init__(self):
+        """Initialize an empty list"""
+        self._vehicles: list[Vehicle] = []
+
+    # Getter
+    @property
+    def vehicles(self) -> list[Vehicle]:
+        """Return a copy of the internal list (to protect encapsulation)"""
+        return list[self._vehicles]
+
+    def add_vehicle(self, vehicle: Vehicle) -> None:
+        """Add a vehicle to the list"""
+        self._vehicles.append(vehicle)
+
+    def empty_garage(self):
+        """Empties the garage of all the vehicles"""
+        self._vehicles.clear()
+
+    # def sort_by_release(self):
+        # self._vehicles.sort
